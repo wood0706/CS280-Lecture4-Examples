5cf8807f5ff386788c1913057fb3fecb877aab55
wood0706 <wood0706@gmail.com>
2017-03-14 19:52:19 +0800

    enum

---
+++ Example01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Status response = Status.Success;
+
+            switch (response)
+            {
+                case Status.Success:
+                    //你要寫的程式
+                    Console.WriteLine("狀態成功");
+                    Console.WriteLine(response);
+
+                    break;
+
+                case Status.Error:
+                    //你要寫的程式
+                    Console.WriteLine("狀態錯誤");
+                    Console.WriteLine(response);
+                    break;
+
+                default:
+                    Console.WriteLine("狀態不明,什麼都不做");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
